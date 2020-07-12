9f1f51b882f83551691a75c01da345db86baf9ba
Joachim Dick <62520542+JoaDick@users.noreply.github.com>
2020-07-12 21:25:17 +0200

    Added some spume to the Warerfall.

--- intern/WaterfallDroplet.h
+++ intern/WaterfallDroplet.h
@@ -47,31 +47,45 @@ namespace EC
       // animation.safePixel(_fadingEndPos * animation.ledCount) = CRGB(0, 255, 0);
 #endif
 
-      if (_volumeMax > 0 &&
-          _pos >= _fadingEndPos)
-      {
-        const float fadingRange = _fadingBeginPos - _fadingEndPos;
-        uint8_t volume = _volumeMax;
-        if (_pos <= _fadingBeginPos)
-        {
-          volume = volume * (_pos - _fadingEndPos) / fadingRange;
-        }
+      const float fadingRange = _fadingBeginPos - _fadingEndPos;
+      uint8_t paletteIndex = uint16_t(255 * (_pos + fadingRange) / _fadingBeginPos) % 255;
+      uint8_t volume = 0;
 
-        const uint8_t paletteIndex = uint16_t(255 * (_pos + fadingRange) / _fadingBeginPos) % 255;
+      if (_pos < _fadingEndPos)
+      {
+        // no drop
+      }
+      else if (_pos <= _fadingBeginPos)
+      {
+        volume = _volumeMax * (_pos - _fadingEndPos) / fadingRange;
+        // add some spume?
+        if (random8() < 1)
+        {
+          paletteIndex = random8();
+          volume = 128;
+        }
+      }
+      else
+      {
+        volume = _volumeMax;
+      }
+
+      if (volume > 0)
+      {
         const CRGB color = ColorFromPalette(OceanColors_p, paletteIndex, volume);
         const int16_t pixelPos = _pos * animation.ledCount;
         animation.safePixel(pixelPos) = color;
+      }
 
 #ifdef WATERFALL_DEBUG
-        Serial.print(F(" fadingBeginPos:"));
-        Serial.print(_fadingBeginPos * 255);
-        Serial.print(F(" fadingEndPos:"));
-        Serial.print(_fadingEndPos * 255);
-        Serial.print(F(" paletteIndex:"));
-        Serial.print(paletteIndex);
-        Serial.println();
+      Serial.print(F(" fadingBeginPos:"));
+      Serial.print(_fadingBeginPos * 255);
+      Serial.print(F(" fadingEndPos:"));
+      Serial.print(_fadingEndPos * 255);
+      Serial.print(F(" paletteIndex:"));
+      Serial.print(paletteIndex);
+      Serial.println();
 #endif
-      }
     }
 
     void update(uint16_t delta_ms)
@@ -95,14 +109,30 @@ namespace EC
       // reached the ground?
       if (_pos <= 0.0)
       {
-        _acc = randomF(-0.75, -0.5);
-        _vel = randomF(0.05, 0.1);
-        _pos = randomF(0.95, 1.0);
+        // bounce-back?
+        if (_fadingEndPos < 0 &&
+            random8() < 75)
+        {
+          _acc = randomF(-2.0, -3.0);
+          _vel = -_vel;
+          _pos = 0.0;
 
-        _vMax = randomF(-0.75, -0.5);
-        _volumeMax = 16 + random8(256-16);
-        _fadingBeginPos = randomF(0.3, 0.9);
-        _fadingEndPos = randomF(-0.2, 0.1);
+          _volumeMax /= 2;
+          _fadingBeginPos = 0.01;
+          _fadingEndPos = 0.0;
+        }
+        // restart
+        else
+        {
+          _acc = randomF(-0.75, -0.5);
+          _vel = randomF(0.05, 0.1);
+          _pos = randomF(0.95, 1.0);
+
+          _vMax = randomF(-0.65, -0.3);
+          _volumeMax = 8 + random8(256 - 8);
+          _fadingBeginPos = randomF(0.3, 0.9);
+          _fadingEndPos = randomF(-0.2, 0.1);
+        }
       }
     }
 
@@ -119,7 +149,7 @@ namespace EC
     uint8_t _volumeMax = 0;
     float _vMax = 0.0;
     float _fadingBeginPos = 1.0;
-    float _fadingEndPos = 0.0;
+    float _fadingEndPos = 0.99;
 
 #ifdef WATERFALL_DEBUG
     float _debugPos_vMax = 0.0;
