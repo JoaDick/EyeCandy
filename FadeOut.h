fbc4e86bd764f39631b3bfcdffc43e1e46e3b509
Joachim Dick <62520542+JoaDick@users.noreply.github.com>
2020-07-25 00:37:49 +0200

    Implemented overlay mode for FadeOut.

--- FadeOut.h
+++ FadeOut.h
@@ -43,17 +43,31 @@ namespace EC
      * Lower value = longer glowing.
      * This setting can be adjusted at runtime.
      */
-    uint8_t fadeRate = fadeRate_default();
-    static uint8_t fadeRate_default() { return 50; }
+    uint8_t fadeRate;
 
     /** Constructor
      * @param ledStrip  The LED strip.
      * @param ledCount  Number of LEDs.
+     * @param fadeRate  Fading speed; Lower value = longer glowing.
      */
     FadeOut(CRGB *ledStrip,
             uint16_t ledCount,
-            uint8_t fadeRate = fadeRate_default())
-        : AnimationBaseFL(TYPE_FADING_PATTERN, ledStrip, ledCount), fadeRate(fadeRate)
+            uint8_t fadeRate)
+        : FadeOut(ledStrip, ledCount, false, fadeRate)
+    {
+    }
+
+    /** Constructor
+     * @param ledStrip  The LED strip.
+     * @param ledCount  Number of LEDs.
+     * @param overlayMode  Set to true when Animation shall be an Overlay.
+     * @param fadeRate  Fading speed; Lower value = longer glowing.
+     */
+    FadeOut(CRGB *ledStrip,
+            uint16_t ledCount,
+            bool overlayMode,
+            uint8_t fadeRate)
+        : AnimationBaseFL(overlayMode ? TYPE_OVERLAY : TYPE_FADING_PATTERN, ledStrip, ledCount), fadeRate(fadeRate)
     {
     }
 
@@ -61,8 +75,14 @@ namespace EC
     /// @see AnimationBase::showPattern()
     uint8_t showPattern(uint32_t currentMillis) override
     {
-      fadeToBlackBy(ledStrip, ledCount, fadeRate);
+      showOverlay(currentMillis);
       return 0;
+    }
+
+    /// @see AnimationBase::showOverlay()
+    void showOverlay(uint32_t currentMillis) override
+    {
+      fadeToBlackBy(ledStrip, ledCount, fadeRate);
     }
   };
 
